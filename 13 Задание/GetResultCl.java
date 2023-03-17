631ef4d36919389915f3529539703d7bddf865a9
Afer1st2022 <113258884+Afer1st2022@users.noreply.github.com>
2023-03-17 11:13:58 +0300

    Add files via upload

---
+++ 13 Задание/GetResultCl.java
@@ -0,0 +1,22 @@
+package tasks.task13;
+import java.util.Scanner;
+public class GetResultCl {
+    public static void GetResult() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите начало ряда, шаг, количество шагов: ");
+        String[] stringArray = in.nextLine().split(" ");
+        int[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+        int start = intArray[0];
+        int step = intArray[1];
+        int times = intArray[2];
+        int count = 0;
+        while (count != times) {
+            System.out.println(start);
+            start += step;
+            count += 1;
+        }
+    }
+}
---
+++ 13 Задание/Main.java
@@ -0,0 +1,7 @@
+package tasks.task13;
+
+public class Main {
+    public static void main(String[] args) {
+        GetResultCl.GetResult();
+    }
+}
